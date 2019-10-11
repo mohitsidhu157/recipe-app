58fd9c1e69ae932353d46583ee4b093d3ec6e530
Mohit kumar <mohitsidhu157@gmail.com>
2019-10-11 20:47:43 +0530

    adding material ui and firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const firebase = require('firebase');
+  require('firebase/firestore');
+  const firebaseConfig = {
+    apiKey: "AIzaSyBn_7QExcAdkJ3NIYUU4phWZnDdY3j-KWc",
+    authDomain: "evernote-clone-ebcdd.firebaseapp.com",
+    databaseURL: "https://evernote-clone-ebcdd.firebaseio.com",
+    projectId: "evernote-clone-ebcdd",
+    storageBucket: "evernote-clone-ebcdd.appspot.com",
+    messagingSenderId: "935880973879",
+    appId: "1:935880973879:web:9b49de37a795673a6529b2",
+    measurementId: "G-C8LWBSH4HK"
+  };
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
