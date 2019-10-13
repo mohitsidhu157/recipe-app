50952b83f10b91790205d15c0052ea530bec26b7
Mohit kumar <mohitsidhu157@gmail.com>
2019-10-13 21:32:58 +0530

    adding recipe component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
-
+import React,{useEffect, useState} from 'react';
+import Recipe from "./Recipe";
 import './App.css';
-const  firebase = require('firebase');
+
 function App () {
     const APP_ID = '8dd921db';
     const APP_KEY = '17c31add41ba209f2afe8e957b511fc9';
-    const exampleReq = 
-    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const [recipes, setRecipes]  = useState([]);
+    const [search,setSearch] = useState('banana'); 
+    const [query, setQuery] = useState("");
+    useEffect(()=>{
+      getRecipe();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[query]);
+    const getRecipe = async ()=>{
+      const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      
+      const data = await res.json();
+      console.log(data.hits);
+      setRecipes(data.hits);
+    }
+    const getSearch = (e) => {
+      e.preventDefault();
+      setQuery(search);
+      setSearch('');
+    }
+    const updateSearch = (e) => {
+      setSearch(e.target.value);
+    }
       return (
       <div className="App">
-      Hello world
+        <form className="search-form" onSubmit={getSearch}>
+          <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
+          <button type="submit" className="search-button">Search</button>
+        </form>
+        {recipes.map((recipe,index)=>{
+          return <Recipe 
+                    key={index} 
+                    calories={recipe.recipe.calories} 
+                    title={recipe.recipe.label} 
+                    image={recipe.recipe.image}
+                    ingredients={recipe.recipe.ingredients}
+                 />;
+        })}
       </div>
     );
  
--- src/index.js
+++ src/index.js
@@ -3,21 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const firebase = require('firebase');
-  require('firebase/firestore');
-  const firebaseConfig = {
-    apiKey: "AIzaSyBn_7QExcAdkJ3NIYUU4phWZnDdY3j-KWc",
-    authDomain: "evernote-clone-ebcdd.firebaseapp.com",
-    databaseURL: "https://evernote-clone-ebcdd.firebaseio.com",
-    projectId: "evernote-clone-ebcdd",
-    storageBucket: "evernote-clone-ebcdd.appspot.com",
-    messagingSenderId: "935880973879",
-    appId: "1:935880973879:web:9b49de37a795673a6529b2",
-    measurementId: "G-C8LWBSH4HK"
-  };
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
