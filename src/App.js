d3be4ec83162170e5e7ddca8ed774a28aea76b3f
Mohit kumar <mohitsidhu157@gmail.com>
2019-10-14 22:55:38 +0530

    setting home page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <ul>
-            <li>Home </li>
-            <li>About Us</li>
-            <li>Recipes </li>
-          </ul>
-      </header>
+      <div className="bg">
+        <header className="App-header">
+            <ul>
+              <li><h2>Home</h2> </li>
+              <li><h2>About Us</h2></li>
+              <li><h2>Recipes</h2> </li>
+            </ul>
+        </header>
+        <main>
+          <div className="search-form">
+          <form >
+            <input type="text" className="search-field"/>
+            <input type="submit" value="search" className="search-button"/>
+          </form>
+          </div>
+         
+        </main>
+      </div>
+      
     </div>
   );
 }
