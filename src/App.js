a005be45c415ed463ae9b67cbb2ee6d62822e9a7
Mohit kumar <mohitsidhu157@gmail.com>
2019-10-14 21:54:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,26 @@
-import React,{useEffect, useState} from 'react';
-import Recipe from "./Recipe";
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-function App () {
-    const APP_ID = '8dd921db';
-    const APP_KEY = '17c31add41ba209f2afe8e957b511fc9';
-    const [recipes, setRecipes]  = useState([]);
-    const [search,setSearch] = useState(''); 
-    const [query, setQuery] = useState("");
-    useEffect(()=>{
-      getRecipe();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[query]);
-    const getRecipe = async ()=>{
-      const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-      
-      const data = await res.json();
-      console.log(data.hits);
-      setRecipes(data.hits);
-    }
-    const getSearch = (e) => {
-      e.preventDefault();
-      setQuery(search);
-      setSearch('');
-    }
-    const updateSearch = (e) => {
-      setSearch(e.target.value);
-    }
-      return (
-      <div className="App">
-        <form className="search-form" onSubmit={getSearch}>
-          <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
-          <button type="submit" className="search-button">Search</button>
-        </form>
-        <div className="Recipes">
-        {recipes.map((recipe,index)=>{
-          return <Recipe 
-                    key={index} 
-                    calories={recipe.recipe.calories} 
-                    title={recipe.recipe.label} 
-                    image={recipe.recipe.image}
-                    ingredients={recipe.recipe.ingredients}
-                 />;
-        })}
-        </div>
-
-      </div>
-    );
- 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
